82980117c8444d50a8bb646290c9929b6b1079ea
Karl Neyvaldt <karl.neyvaldt@meltwater.com>
2016-09-30 10:08:11 +0200

    inverted not set SNAPSHOT variable meaning

--- upload_to_bintray.sh
+++ upload_to_bintray.sh
@@ -9,6 +9,7 @@ if [ -n "${TRAVIS_TAG}" ]; then
         echo "BINTRAY_KEY is unset or set to the empty string"
         exit 1
     fi
+    SNAPSHOT=false
 	./gradlew bintray
 else
     echo "Will not upload to Bintray."
